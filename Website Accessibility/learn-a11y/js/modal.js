dbddc4791880ea2ff26ad9a5f3605f0da021450c
ZakBrinlee <zbrinlee@hotmail.com>
2020-04-08 22:40:19 -0700

    Focus control and practice

--- Website Accessibility/learn-a11y/js/modal.js
+++ Website Accessibility/learn-a11y/js/modal.js
@@ -1,22 +1,63 @@
-var modal        = document.querySelector('.focus-modal');
-var modalButton  = document.querySelector('.focus-modal-button');
-var modalOverlay = document.querySelector('.focus-modal-overlay');
-var cancelButton = document.querySelector('.focus-modal-cancel');
+var modal = document.querySelector(".focus-modal");
+var modalButton = document.querySelector(".focus-modal-button");
+var modalOverlay = document.querySelector(".focus-modal-overlay");
+var cancelButton = document.querySelector(".focus-modal-cancel");
 
-modalButton.addEventListener('click', open);
-cancelButton.addEventListener('click', close);
-
-// Get a list of tabbable elements here:
-// https://github.com/jkup/focusable
+modalButton.addEventListener("click", open);
+cancelButton.addEventListener("click", close);
 
 function open() {
+  // Initialize the element that was focused prior to modal open
+  var previouslyFocused = document.activeElement;
+
+  // Add event listener using keydown and custom function
+  modal.addEventListener("keydown", trapTabKey);
+
+  // Add event listener for click
+  modalOverlay.addEventListener('click', close);
+
+  // Get list of all focusable elements
+  var focusableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]):not([type="hidden"]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable], audio[controls], video[controls], summary, [tabindex^="0"], [tabindex^="1"], [tabindex^="2"], [tabindex^="3"], [tabindex^="4"], [tabindex^="5"], [tabindex^="6"], [tabindex^="7"], [tabindex^="8"], [tabindex^="9"]');
+
+  focusableElements = Array.prototype.slice.call(focusableElements);
+
+  // Initialize first and last focusable elements
+  var firstItem = focusableElements[0];
+  var lastItem = focusableElements[focusableElements.length - 1];
+
   // Show the modal and overlay
-  modal.style.display = 'block';
-  modalOverlay.style.display = 'block';
+  modal.style.display = "block";
+  modalOverlay.style.display = "block";
+
+  // Auto focus on first item
+  firstItem.focus();
+
+  function trapTabKey(event) {
+    if (event.keyCode === 9) {
+      // Shift tab
+      if (event.shiftKey) {
+        // Backwards
+        if (document.activeElement === firstItem) {
+          event.preventDefault();
+          lastItem.focus();
+        }
+        // Tab
+      } else {
+        // Forwards
+        if (document.activeElement === lastItem) {
+          event.preventDefault();
+          firstItem.focus();
+        }
+      }
+      // Escape
+    } else if (event.keyCode === 27) {
+      close();
+    }
+  }
 }
 
 function close() {
   // Hide the modal and overlay
-  modal.style.display = 'none';
-  modalOverlay.style.display = 'none';
+  modal.style.display = "none";
+  modalOverlay.style.display = "none";
 }
